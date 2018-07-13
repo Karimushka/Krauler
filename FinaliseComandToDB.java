8ea38d8a5c022e5296254a9895a849f63a68a314
Karimushka <20karim11@mail.ru>
2018-07-13 13:46:27 +0700

    Небольшие изменения в запросах SQL, xml-ина перенесена

--- FinaliseComandToDB.java
+++ FinaliseComandToDB.java
@@ -3,8 +3,8 @@ public interface FinaliseComandToDB {
     String UPDATE_REQUEST = "UPDATE pages SET lastScanDAte = current_timestump() WHERE id = ?;";
     String INSERT_REQUEST = "INSERT INTO pages (url, siteID, foundDateTime) VALUES (?, ?, ?);";
     String COUNT_REQUEST_FOR_METHOD_GETSITEIDWHIS = "SELECT sites.ID, count * FROM sites, pages WHERE pages.siteID = sites.ID GROUP BY sites.ID HAVING count * = 1;";
-    String SELECT_REQUEST_FOR_METHOD_ROOTSSITES = "SELECT ID, siteID, url FROM pages WHERE url LIKE '%robots.txt' AND lastScanDate = NULL;";
-    String SELECT_REQUEST_FOR_METHOD_GETIDURL = "SELECT ID, url FROM pages WHERE siteID = ? AND lastScanDate = NULL;";
-    String SELECT_REQUEST_FOR_METHOD_ADD_SITEMAPSANDART = "SELECT ID, siteID, url FROM pages WHERE url LIKE '%sitemap%' AND lastScanDate = NULL;";
+    String SELECT_REQUEST_FOR_METHOD_ROOTSSITES = "SELECT ID, siteID, url FROM pages WHERE url LIKE '%robots.txt' AND lastScanDate IS NULL;";
+    String SELECT_REQUEST_FOR_METHOD_GETIDURL = "SELECT ID, url FROM pages WHERE siteID = ? AND lastScanDate IS NULL;";
+    String SELECT_REQUEST_FOR_METHOD_ADD_SITEMAPSANDART = "SELECT ID, siteID, url FROM pages WHERE url LIKE '%sitemap%' AND lastScanDate IS NULL;";
     String SELECT_REQUEST_FOR_METHOD_GETCOUNT_PAGES = "SELECT count * FROM pages WHERE url NOT LIKE '%sitemap%' AND url NOT LIKE '%robots.txt%';";
 }
